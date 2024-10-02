87a7005d52dc372236ddb0942e61cd53b1631f4b
luc1aol <luciolivera1802@gmail.com>
2024-10-02 16:10:26 -0300

    Id autoincremental a los Pedidos y seccione el código

--- Pedidos.py
+++ Pedidos.py
@@ -1,1 +1,26 @@
 # Acá van a ir las funciones y todo lo relacionado con el apartado pedidos.
+import random
+
+# Inicializamos el primer pedido con un valor de id=10
+idPedidoActual=10
+
+
+# Acá va una función para hallar la fecha actual y asignarla al pedido
+#   Las fechas que necesitamos en el pedido son la fecha en que se confirmó el pedido y la de entrega 
+# 
+
+def nuevoPedido(id_cliente, productos):
+
+    id_pedido = idPedidoActual
+
+    # Lo hacemos autoincremenetal
+    idPedidoActual +=1
+    
+    # Bloque para anclar la fecha actual al pedido
+    # 
+    # 
+    
+    # Bloque para asignar la cantidad de los productos 
+
+    # Vamos a tener que retornar un diccionario que contenga toda la info junta
+    return
