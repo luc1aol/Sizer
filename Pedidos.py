2271c395b7e3a05554e89b09687642003d287773
Agustín Toranza <agtoranza@gmail.com>
2024-10-04 15:10:33 -0300

    aaaaa

--- Pedidos.py
+++ Pedidos.py
@@ -5,10 +5,18 @@ import random
 idPedidoActual=10
 
 
-# Acá va una función para hallar la fecha actual y asignarla al pedido
-#   Las fechas que necesitamos en el pedido son la fecha en que se confirmó el pedido y la de entrega 
-# 
-
+def fecha():
+    #Detectar que el pedido esta confirmado 
+    aux = time.localtime()
+    año = aux.tm_year
+    mes = aux.tm_mon
+    dia = aux.tm_mday
+    hora = aux.tm_hour
+    minuto = aux.tm_min
+    seg = aux.tm_sec
+    fechaPedido = str(dia)+ "/" + str(mes) + "/" + str(año) + " " + str(hora) + ":" + str(minuto) + ":" + str(seg)
+    fechaEntrega = None  #¿Cómo calcular el plazo de entrega de cada producto?
+    
 # Esta función recibe id del cliente y un diccionario de diccionarios
 # {{{producto1},{cantidad}},{producto2},{cantidad}}
 def nuevoPedido(cliente, producto_cantidad):
@@ -18,9 +26,7 @@ def nuevoPedido(cliente, producto_cantidad):
     # Lo hacemos autoincremenetal
     idPedidoActual +=1
     
-    # Bloque para anclar la fecha actual al pedido
-    # 
-    # 
+    fechaActual = fecha()
     
     # Bloque para extraer los datos del diccionario producto_cantidad que pedimos por parametro
     # 
