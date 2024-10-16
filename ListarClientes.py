72ba23e262c6cc1cd4bb702958d2d9ec2e986402
luc1aol <luciolivera1802@gmail.com>
2024-10-15 22:39:54 -0300

    Arreglé el listar clientes

--- ListarClientes.py
+++ ListarClientes.py
@@ -3,5 +3,5 @@ def listarClientes(dicClientes):
         print("No se encuentran clientes registrados.")
     else:
         for dni, detalles in dicClientes.items():
-            print(f"DNI: {dni}, Nombre: {detalles['nombre']}, Dirección: {detalles['domicilio']}, Teléfono: {detalles['telefono']}")
+            print(f"DNI: {dni}, Nombre: {detalles['nombre']}, Domicilio: {detalles['domicilio']}, Teléfono: {detalles['telefono']}")
     return
--- Validar.py
+++ Validar.py
@@ -2,13 +2,13 @@ import re
 
 def dniValido(accion):
     while True:
-        dni = int(input(f"Ingrese el DNI del cliente para {accion} (0 para finalizar): "))
+            dni = input(f"Ingrese el DNI del cliente para {accion} (0 para finalizar): ")
 
-        if dni == 0:
-            return dni
-        
-        if re.match(r'^\d{8}$', str(dni)):
-            return dni
-        else:
-            print("Error: El DNI debe tener 8 dígitos numéricos. Inténtelo de nuevo.")
+            if dni == 0:
+                return int(dni)
+            
+            if re.match(r'^\d{8}$', str(dni)):
+                return int(dni)
+            else:
+                print("Error: El DNI debe tener 8 dígitos numéricos. Inténtelo de nuevo.")
             
