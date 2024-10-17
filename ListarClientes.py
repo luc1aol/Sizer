9fdd1a4fb2637bc9bf704d22778952940772a27f
maria paz traversi <mariapaztraversi@gmail.com>
2024-10-17 18:11:40 -0300

    documente ListarPedidos

--- ListarClientes.py
+++ ListarClientes.py
@@ -1,3 +1,7 @@
+# Comprobamos si el diccionario de clientes está vacío
+# Si el diccionario está vacío, mostramos un mensaje indicando que no hay clientes registrados
+# Si el diccionario no está vacío, recorremos cada cliente en el diccionario
+
 def listarClientes(dicClientes):
     print("-----------------------------------\n LISTADO CLIENTES\n-----------------------------------")
     if not dicClientes:
