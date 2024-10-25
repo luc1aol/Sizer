fcb21d754803418de02ddfbad6889f613dccc5fb
luc1aol <luciolivera1802@gmail.com>
2024-10-25 15:53:32 -0300

    Cambioss

--- ActualizarCliente.py
+++ ActualizarCliente.py
@@ -18,11 +18,13 @@ def actualizarCliente(dicClientes):
         for dni, detalles in dicClientes.items():
             print(f"Nombre: {detalles['nombre']}")
             print(f"Domicilio: {detalles['domicilio']}")
+            print(f"Apellido: {detalles['apellido']}")
             print(f"Teléfono: {detalles['telefono']}")
             
         # Solicitar nuevos datos
         nuevos_datos = {
             "nombre": input("Ingrese el nuevo nombre del cliente (dejar en blanco para no cambiar): "),
+            "apellido": input("Ingrese el nuevo apellido del cliente (dejar en blanco para no cambiar): "),
             "domicilio": input("Ingrese la nueva dirección del cliente (dejar en blanco para no cambiar): "),
             "telefono": input("Ingrese el nuevo teléfono del cliente (dejar en blanco para no cambiar): ")
         }   
